07ed897b79a895faef4d7c95fdc6f6b09e778d43
Tetsuya Shiota <tetsuya.shiota.1231@gmail.com>
2018-07-21 03:42:05 +0900

    add_cmake

---
+++ AI/core/SampleAI.cpp
@@ -0,0 +1,20 @@
+//
+// Created by Tetsuya Shiota on 2018/07/21.
+//
+
+#include <vector>
+#include "../TraceDumper/TraceDumper.hpp"
+
+
+
+int main(){
+
+    /* モデルの内容受け取る */
+
+    vector<Command> commands;
+    /* AIの処理 */
+
+    dumpTrace(commands);
+
+
+}
