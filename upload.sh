6500f65807fea0c1852e7ab1f17da370da083df9
Shingo INADA <mecha.is06@gmail.com>
2018-07-21 14:14:38 +0900

    update uploadsh

--- upload.sh
+++ upload.sh
@@ -2,7 +2,7 @@
 prob=$1
 file=$2
 
-http_code=$(curl -v --compressed -w '%{http_code}' -F "solution=@${file}" "localhost:5000/upload/${prob}")
+http_code=$(curl -v --compressed -w '%{http_code}' -F "solution=@${file}" "http://18.179.226.203:5000/upload/${prob}")
 
 if [[ $http_code -eq 200 ]]; then
   exit 0
