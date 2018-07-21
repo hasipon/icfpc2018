c5495749ef6e784c92a7388170f67b58e75d52fd
KINOSHITA Minoru <kinoshita.minoru22@gmail.com>
2018-07-21 19:04:12 +0900

    check script

--- .jenkins/script.sh
+++ .jenkins/script.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
-# check simulator
-./simulator/check.sh
-
-# update repository
 pushd /home/ubuntu/icfpc2018
-git pull origin master
+
+  # update repository
+  git pull origin master
+
+  pushd simulator
+    go build -o simulator
+  popd
+
+  ./validate.sh
+
 popd
