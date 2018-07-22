166dd63d270afddd5425a9970ebb69ccfb22b3ce
hasi <hasipon@users.noreply.github.com>
2018-07-22 20:57:00 +0000

    make .sc6

--- out/make.pl
+++ out/make.pl
@@ -5,9 +5,13 @@ while(<>){
 	chomp;
 	if (m|^\.\./nbts/([^_]+)_(.*)|) {
 		my $s = "$1/$2.gz";
+		my $t = "$1/$2.sc6";
+		my $p = $2;
+		$p =~ s/.nbt$//;
 		$d{$1} = 1;
-		print " $s";
+		print " $s $t";
 		push(@a, "$s: $_\n\tgzip -c $_ >$s\n");
+		push(@a, "$t: $_\n\t../tracer6 ../problemsF/${p}_src.mdl ../problemsF/${p}_tgt.mdl < $_ >$t\n");
 	}
 }
 print "\n";
