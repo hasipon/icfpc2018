b46af57eac0996565630b4389a6dd5d8f2655b4c
Tetsuya Shiota <tetsuya.shiota.1231@gmail.com>
2018-07-22 17:50:11 +0900

    ExecuteTrace

--- ExecuteTrace/ExecuteTrace.go
+++ ExecuteTrace/ExecuteTrace.go
@@ -4,30 +4,40 @@ import (
 	"log"
 	"github.com/sclevine/agouti"
 	"flag"
+	"os"
+	"time"
+	"fmt"
+	"strings"
 )
+func Exists(filename string) bool {
+	_, err := os.Stat(filename)
+	return err == nil
+}
 
 func main() {
 	flag.Parse()
 
-	/*
 	 src_file := flag.Arg(0)
 	tgt_file := flag.Arg(1)
 	trace_file := flag.Arg(2)
-	*/
+
 	if flag.NArg() != 3{
 		log.Fatal("Usage: ./ExecuteTrace {src_file} {tgt_file} {trace_file}")
 	}
 
 	url := "http://18.179.226.203/ExecuteTrace/official.html"
 
-	driver := agouti.ChromeDriver()
+	driver := agouti.ChromeDriver(
+		agouti.ChromeOptions("args", []string{"--headless", "--disable-gpu"}),
+
+	)
 	err := driver.Start()
 	if err != nil {
 		log.Printf("Failed to start driver: %v", err)
 	}
 	defer driver.Stop()
 
-	page, err := driver.NewPage(agouti.Browser("chrome"))
+	page, err := driver.NewPage()
 	if err != nil {
 		log.Printf("Failed to open page: %v", err)
 	}
@@ -37,12 +47,41 @@ func main() {
 		log.Printf("Failed to navigate: %v", err)
 	}
 
-	// contentにHTMLが入る
-	content, err := page.HTML()
-	if err != nil {
-		log.Printf("Failed to get html: %v", err)
+	src_file_form := page.FindByID("srcModelFileIn")
+	src_file_form_empty := page.FindByID("srcModelEmpty")
+	tgt_file_form := page.FindByID("tgtModelFileIn")
+	tgt_file_form_empty := page.FindByID("tgtModelEmpty")
+	trace_file_form := page.FindByID("traceFileIn")
+
+	submit_butoon := page.FindByID("execTrace")
+
+	if !Exists(src_file) {
+		src_file_form_empty.Click()
+	} else {
+		src_file_form.UploadFile(src_file)
 	}
-	log.Println(content)
 
+	if !Exists(tgt_file) {
+		tgt_file_form_empty.Click()
+	} else {
+		tgt_file_form.UploadFile(tgt_file)
+	}
 
+	trace_file_form.UploadFile(trace_file)
+
+	submit_butoon.Click()
+
+	output := page.FindByID("stdout")
+	for i:=0; i< 10; i++{
+		text, err := output.Text()
+		if err != nil {
+			log.Printf("Failed to get html: %v", err)
+		}
+		if strings.Contains(text, "Failure") || strings.Contains(text, "Success"){
+			fmt.Println(text)
+			os.Exit(0)
+		}
+		time.Sleep(1 * time.Second)
+	}
+	os.Exit(1)
 }
