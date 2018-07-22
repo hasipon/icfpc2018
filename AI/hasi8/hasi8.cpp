623849fe43e2c856864e2fd542493ae50c33cf9d
hasi <hasipon@users.noreply.github.com>
2018-07-22 04:21:30 +0000

    Filled

--- AI/hasi8/hasi8.cpp
+++ AI/hasi8/hasi8.cpp
@@ -1,15 +1,30 @@
+#include "../../include/point.hpp"
 #include "../../include/model.hpp"
 #include "../../include/output.hpp"
 #include <stdio.h>
 #include <algorithm>
+#include <vector>
 
 using namespace std;
+
+typedef Point P;
+
+typedef vector<uint64_t> Filled;
 
 struct Main : OutputBase {
 	const Model& Src, Tgt;
 	const int R;
 	Main(const Model& Src, const Model& Tgt, std::ofstream& ofs) : OutputBase(ofs), Src(Src), Tgt(Tgt), R(max(Src.R, Tgt.R)) {}
 	void solve();
+	Filled newFilled() { return Filled((R*R*R+63)/64); }
+	void set(Filled a, P p) {
+		int x = (p.x * R + p.y) * R + p.z;
+		a[x / 64] |= 1ULL << (x % 64);
+	}
+	bool get(Filled a, P p) const {
+		int x = (p.x * R + p.y) * R + p.z;
+		return (a[x / 64] >> (x % 64)) & 1;
+	}
 };
 
 void Main::solve() {
