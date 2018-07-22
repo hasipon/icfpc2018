a686829b57864e93ea46163ee956edea96d62cdf
Tetsuya Shiota <tetsuya.shiota.1231@gmail.com>
2018-07-22 13:06:48 +0900

    add command interface

--- include/output.hpp
+++ include/output.hpp
@@ -1,7 +1,18 @@
 #ifndef _OUTPUT_H_
 #define _OUTPUT_H_
 
+#include "point.hpp"
 #include <fstream>
+
+int8_t to_nd(const Point &c) {
+	int dx = c.x;
+	int dy = c.y;
+	int dz = c.z;
+	assert(-1 <= dx && dx <= 1);
+	assert(-1 <= dy && dy <= 1);
+	assert(-1 <= dz && dz <= 1);
+	return (dx + 1) * 9 + (dy + 1) * 3 + (dz + 1);
+}
 
 struct OutputBase {
 	std::ofstream& ofs;
@@ -12,6 +23,44 @@ struct OutputBase {
 		const char buf[1] = {(char)0b11111111};
 		ofs.write(buf, 1);
 	}
+	void Wait(){
+		const char buf[1] = {(char)0b11111110};
+		ofs.write(buf, 1);
+	}
+	void Flip(){
+		const char buf[1] = {(char)0b11111101};
+		ofs.write(buf, 1);
+	}
+	void SMove(Point to){
+
+	}
+	void LMove(Point via, Point to){
+
+	}
+
+	void FusionP(Point with){
+
+	}
+	void FusionS(Point with){
+
+	}
+	void Fission(Point to, int8_t m){
+
+	}
+	void Fill(Point to){
+
+	}
+	void Void(Point to){
+
+	}
+	void GFill(Point vertex, Point region){
+
+	}
+	void GVoid(Point vertex, Point region){
+
+	}
+
+
 };
 
 #endif /* _OUTPUT_H_ */
