2108763e834c758b1e9e1d14d39da53f600e2081
Tetsuya Shiota <tetsuya.shiota.1231@gmail.com>
2018-07-22 17:10:48 +0900

    ExecuteTrace

---
+++ ExecuteTrace/ExecuteTrace.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"log"
+	"github.com/sclevine/agouti"
+	"flag"
+)
+
+func main() {
+	/*
+	 src_file := flag.Arg(0)
+	tgt_file := flag.Arg(1)
+	trace_file := flag.Arg(2)
+	*/
+	if flag.NArg() != 2{
+		log.Fatal("Usage: ./ExecuteTrace {src_file} {tgt_file} {trace_file}")
+	}
+
+	url := "http://18.179.226.203/ExecuteTrace/official.html"
+
+	driver := agouti.ChromeDriver()
+	err := driver.Start()
+	if err != nil {
+		log.Printf("Failed to start driver: %v", err)
+	}
+	defer driver.Stop()
+
+	page, err := driver.NewPage(agouti.Browser("chrome"))
+	if err != nil {
+		log.Printf("Failed to open page: %v", err)
+	}
+
+	err = page.Navigate(url)
+	if err != nil {
+		log.Printf("Failed to navigate: %v", err)
+	}
+
+	// contentにHTMLが入る
+	content, err := page.HTML()
+	if err != nil {
+		log.Printf("Failed to get html: %v", err)
+	}
+	log.Println(content)
+
+
+}
