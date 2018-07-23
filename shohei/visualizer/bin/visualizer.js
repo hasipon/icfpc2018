893dc5608f7fa6c92b071949adbe3cfd5a1c451a
shohei909 <909mmsphere@gmail.com>
2018-07-23 13:44:28 +0900

    visualizer更新

--- shohei/visualizer/bin/visualizer.js
+++ shohei/visualizer/bin/visualizer.js
@@ -535,11 +535,38 @@ Game.prototype = {
 		this.step++;
 	}
 	,forward: function(command) {
+		if(this.halted) {
+			throw new js__$Boot_HaxeError("すでにHaltしてます");
+		}
 		var bot = this.bots[this.botIndex];
 		var _g = _$Command_Command_$Impl_$.kind(command);
 		switch(_g) {
 		case 0:
 			this.halted = true;
+			if(this.getActiveBotsCount() != 1) {
+				throw new js__$Boot_HaxeError("ボットが複数の状態でhaltしました:" + this.getActiveBotsCount());
+			}
+			if((bot.position & 255) != 0 || (bot.position >> 8 & 255) != 0 || (bot.position >> 16 & 255) != 0) {
+				throw new js__$Boot_HaxeError("原点以外でhaltしました:" + (bot.position & 255) + "," + (bot.position >> 8 & 255) + "," + (bot.position >> 16 & 255));
+			}
+			var _g1 = 0;
+			var _g2 = this.size;
+			while(_g1 < _g2) {
+				var x = _g1++;
+				var _g3 = 0;
+				var _g21 = this.size;
+				while(_g3 < _g21) {
+					var y = _g3++;
+					var _g5 = 0;
+					var _g4 = this.size;
+					while(_g5 < _g4) {
+						var z = _g5++;
+						if(this.currentModel[x][y][z] != this.targetModel[x][y][z]) {
+							throw new js__$Boot_HaxeError("モデルが完成してない状態でHaltしました:" + x + "," + y + "," + z);
+						}
+					}
+				}
+			}
 			break;
 		case 1:
 			this.highHarmonics = !this.highHarmonics;
@@ -608,19 +635,19 @@ Game.prototype = {
 			var far = _$Command_Command_$Impl_$.far(command);
 			if(_$Far_Far_$Impl_$.isPositive(far)) {
 				var pos = _$Position_Position_$Impl_$.near(bot.position,_$Command_Command_$Impl_$.nd(command));
-				var _g1 = 0;
-				var _g2 = (far & 255) - 30 + 1;
-				while(_g1 < _g2) {
-					var x = _g1++;
-					var _g3 = 0;
-					var _g21 = (far >> 8 & 255) - 30 + 1;
-					while(_g3 < _g21) {
-						var y = _g3++;
-						var _g5 = 0;
-						var _g4 = (far >> 16 & 255) - 30 + 1;
-						while(_g5 < _g4) {
-							var z = _g5++;
-							this.fill(_$Position_Position_$Impl_$.moveXyz(pos,x,y,z));
+				var _g11 = 0;
+				var _g6 = (far & 255) - 30 + 1;
+				while(_g11 < _g6) {
+					var x1 = _g11++;
+					var _g31 = 0;
+					var _g22 = (far >> 8 & 255) - 30 + 1;
+					while(_g31 < _g22) {
+						var y1 = _g31++;
+						var _g51 = 0;
+						var _g41 = (far >> 16 & 255) - 30 + 1;
+						while(_g51 < _g41) {
+							var z1 = _g51++;
+							this.fill(_$Position_Position_$Impl_$.moveXyz(pos,x1,y1,z1));
 						}
 					}
 				}
@@ -630,19 +657,19 @@ Game.prototype = {
 			var far1 = _$Command_Command_$Impl_$.far(command);
 			if(_$Far_Far_$Impl_$.isPositive(far1)) {
 				var pos1 = _$Position_Position_$Impl_$.near(bot.position,_$Command_Command_$Impl_$.nd(command));
-				var _g11 = 0;
-				var _g6 = (far1 & 255) - 30 + 1;
-				while(_g11 < _g6) {
-					var x1 = _g11++;
-					var _g31 = 0;
-					var _g22 = (far1 >> 8 & 255) - 30 + 1;
-					while(_g31 < _g22) {
-						var y1 = _g31++;
-						var _g51 = 0;
-						var _g41 = (far1 >> 16 & 255) - 30 + 1;
-						while(_g51 < _g41) {
-							var z1 = _g51++;
-							this["void"](_$Position_Position_$Impl_$.moveXyz(pos1,x1,y1,z1));
+				var _g12 = 0;
+				var _g7 = (far1 & 255) - 30 + 1;
+				while(_g12 < _g7) {
+					var x2 = _g12++;
+					var _g32 = 0;
+					var _g23 = (far1 >> 8 & 255) - 30 + 1;
+					while(_g32 < _g23) {
+						var y2 = _g32++;
+						var _g52 = 0;
+						var _g42 = (far1 >> 16 & 255) - 30 + 1;
+						while(_g52 < _g42) {
+							var z2 = _g52++;
+							this["void"](_$Position_Position_$Impl_$.moveXyz(pos1,x2,y2,z2));
 						}
 					}
 				}
@@ -725,7 +752,7 @@ Game.prototype = {
 		var _g = _$Command_Command_$Impl_$.kind(command);
 		switch(_g) {
 		case 0:
-			return BackwardCommand.Empty;
+			return BackwardCommand.Halt;
 		case 1:
 			return BackwardCommand.Flip;
 		case 2:
@@ -993,7 +1020,7 @@ Game.prototype = {
 						var localGrounded = this.isGrounded(dx1,dy1,dz2,sizeX,sizeY,sizeZ);
 						if(!localGrounded) {
 							this.grounded = false;
-							haxe_Log.trace(x1,{ fileName : "Game.hx", lineNumber : 702, className : "Game", methodName : "resetUnionFind", customParams : [y1,z1,dx1,dy1,dz2]});
+							haxe_Log.trace(x1,{ fileName : "Game.hx", lineNumber : 730, className : "Game", methodName : "resetUnionFind", customParams : [y1,z1,dx1,dy1,dz2]});
 							return;
 						}
 					}
